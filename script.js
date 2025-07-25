d6244a2ee82207645d48f7aa43896f20bbf03e3c
JadIg <sajad21lilo@gmail.com>
2025-07-25 02:24:10 -0700

    getHumanChoice and playRound functions done

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function getComputerChoice() {
     return choices[randomIndex];
 }
 
+function getHumanChoice() {
+    const choice = prompt("Enter rock, paper, or scissors:").toLowerCase();
+    return choice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    }
+    if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')) {
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}.`;
+    } else {
+        computerScore++;
+        return `You lose! ${computerChoice} beats ${humanChoice}.`;
+    }
+}
+
